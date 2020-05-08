b6f7d88e12071cecc073225a5e5b0f878e3b0d6e
bquantump <bquantump@users.noreply.github.com>
2020-05-08 11:17:02 -0700

    Set exe to bash for linux/mac

--- clidev/utils.py
+++ clidev/utils.py
@@ -26,11 +26,11 @@ def unix_edit(azure_config_path, dot_azure_config):
     activate_path = os.path.join(azure_config_path, cli.UN_BIN,
                                  cli.UN_ACTIVATE)
     content = open(activate_path, "r").readlines()
-    
+
     # check if already ran setup before
     if cli.AZ_CONFIG_DIR not in content[0]:
-        content = [cli.AZ_CONFIG_DIR + '=' + dot_azure_config,
-                cli.UN_EXPORT + ' ' + cli.AZ_CONFIG_DIR] + content
+        content = [cli.AZ_CONFIG_DIR + '=' + dot_azure_config + '\n',
+                   cli.UN_EXPORT + ' ' + cli.AZ_CONFIG_DIR + '\n'] + content
         with open(activate_path, "w") as file:
             file.writelines(content)
 
@@ -41,7 +41,7 @@ def bat_edit(azure_config_path, dot_azure_config):
     content = open(activate_path, "r").readlines()
     if cli.AZ_CONFIG_DIR not in content[1]:
         content = content[0:1] + ['set ' + cli.AZ_CONFIG_DIR +
-                                '=' + dot_azure_config] + content[1::]
+                                  '=' + dot_azure_config + '\n'] + content[1::]
         with open(activate_path, "w") as file:
             file.writelines(content)
 
@@ -65,18 +65,20 @@ def ps1_edit(azure_config_path, dot_azure_config):
 def install_cli(cli_path, venv_path):
     venv_path = os.environ[cli.VIRTUAL_ENV] = venv_path
     src_path = os.path.join(cli_path, 'src')
-    activate_path = os.path.join(venv_path, 'Scripts', 'activate') if cli.IS_WINDOWS else 'source ' + os.path.join(
+    activate_path = os.path.join(venv_path, 'Scripts', 'activate') if cli.IS_WINDOWS else '.\ ' + os.path.join(
         venv_path, cli.UN_BIN, cli.UN_ACTIVATE)
     delimiter = ' && ' if cli.IS_WINDOWS else '; '
+    executable = None if cli.IS_WINDOWS else cli.BASH_EXE
+    print("\nactivate path is " + str(activate_path))
     subprocess.call(activate_path + delimiter +
-                    'pip install azure-common', shell=True)
+                    'pip install azure-common', shell=True, executable=None)
     subprocess.call(activate_path + delimiter + cli.PIP_E_CMD +
-                    os.path.join(src_path, 'azure-cli-nspkg'), shell=True)
+                    os.path.join(src_path, 'azure-cli-nspkg'), shell=True, executable=executable)
     subprocess.call(activate_path + delimiter + cli.PIP_E_CMD +
-                    os.path.join(src_path, 'azure-cli-telemetry'), shell=True)
+                    os.path.join(src_path, 'azure-cli-telemetry'), shell=True, executable=executable)
     subprocess.call(activate_path + delimiter + cli.PIP_E_CMD +
-                    os.path.join(src_path, 'azure-cli-core'), shell=True)
+                    os.path.join(src_path, 'azure-cli-core'), shell=True, executable=executable)
     subprocess.call(activate_path + delimiter + cli.PIP_E_CMD +
-                    os.path.join(src_path, 'azure-cli'), shell=True)
+                    os.path.join(src_path, 'azure-cli'), shell=True, executable=executable)
     subprocess.call(activate_path + delimiter + cli.PIP_E_CMD +
-                    os.path.join(src_path, 'azure-cli-testsdk'), shell=True)
+                    os.path.join(src_path, 'azure-cli-testsdk'), shell=True, executable=executable)
